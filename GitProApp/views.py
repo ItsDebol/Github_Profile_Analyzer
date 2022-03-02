ab804e670408e53cb40fb8dcbaec0e30aad2c533
Yosef Emiru <josephnewgold1@gmail.com>
2022-03-03 01:22:18 +0300

    accesing my github profile via githubAPI

--- GitProApp/views.py
+++ GitProApp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+import requests
+import json
 
-# Create your views here.
+def index(request):
+    yoko = requests.get('https://api.github.com/users/ItsDebol')
+    content = yoko.text
+    return HttpResponse(content)
