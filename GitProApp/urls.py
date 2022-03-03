9fe19403f573d989130ae4a17ede5fbf5689bb9b
Yosef Emiru <josephnewgold1@gmail.com>
2022-03-03 17:06:12 +0300

    rendering homepage in the /profiledetails/intro/ address

--- GitProApp/urls.py
+++ GitProApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('expand/', views.index, name='index'),
+    path('intro/', views.rendering, name='rendering')
 ]
--- GitProApp/views.py
+++ GitProApp/views.py
@@ -6,3 +6,6 @@ def index(request):
     yoko = requests.get('https://api.github.com/users/ItsDebol')
     content = yoko.text
     return HttpResponse(content)
+
+def rendering(request):
+    return render(request,'index.html')
