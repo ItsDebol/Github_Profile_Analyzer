419e9efc1a057afdcfb81c0f5876849df2d1cb9c
Yosef Emiru <josephnewgold1@gmail.com>
2022-03-04 04:24:20 +0300

    added get_user function on views.py

--- GitProApp/urls.py
+++ GitProApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('expand/', views.index, name='index'),
-    path('intro/', views.rendering, name='rendering')
+    path('intro/', views.rendering, name='rendering'),
+    path('profile/', views.get_user, name='get_user')
+    
 ]
--- GitProApp/views.py
+++ GitProApp/views.py
@@ -1,11 +1,31 @@
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
+from django.contrib import sessions
 import requests
 import json
+from multiprocessing.pool import ThreadPool
+
 
 def index(request):
-    yoko = requests.get('https://api.github.com/users/ItsDebol')
-    content = yoko.text
-    return HttpResponse(content)
+    response = requests.get('https://api.github.com/users/ItsDebol')  
+    content = response.json()
+    return JsonResponse(content, safe=False)
+   
 
-def rendering(request):
-    return render(request,'index.html')
+
+
+def rendering(request): 
+    if 'data' not in sessions:
+        return render("index.html")
+    else:
+        return render("main.html",data=sessions['data'],graph_y=sessions['graph_y'])
+    
+    
+    
+    
+def get_user(request) -> json:
+    response = requests.get('https://api.github.com/users/ItsDebol')  
+    content = response.json()
+    userinfo = {"name": content["name"],"email":content["email"], "bio":content["bio"], "followers":content["followers"], "following":content["following"], "Total Number of Public Repos":content["public_repos"]  }
+    return JsonResponse(userinfo)
+    
